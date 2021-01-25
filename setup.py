ae00eb7f17e1e92b0912e243c7e4e16a245b5df3
Jordan Martin <jordanma36@gmail.com>
2021-01-25 19:29:07 +0000

    added keywords to setup

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="asyncio-gsm-modem-sinusoidal",
+    name="asyncio-gsm-modem",
     version="0.0.1",
     author="sinusoidal",
     author_email="",
     description="An asyncio GSM modem driver library",
+    keywords=['gsm','modem','asyncio','async','lte','sms','text','usb','serial','phone','mobile','messaging']
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Sinusoidal36/asyncio-gsm-modem",
