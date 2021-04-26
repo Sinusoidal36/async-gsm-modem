5d3e5c9fe8c3140f6b5b9a1f6a72a51c2c379350
Jordan Martin <jordan.martin@callfire.com>
2021-04-26 05:41:28 +0000

    refactored reading messages and SMS model

--- async_gsm_modem/quectel_ec25/modem.py
+++ async_gsm_modem/quectel_ec25/modem.py
@@ -2,10 +2,12 @@ from ..base.modem import ATModem
 from ..base.command import Command, ExtendedCommand
 from ..base.pdu import encodeSmsSubmitPdu, encodeGsm7
 from .sms import SMS
-from .constants import STATUS_MAP, DELETE_FLAG, UNSOLICITED_RESULT_CODES
+from .constants import STATUS_MAP, STATUS_MAP_R, DELETE_FLAG, UNSOLICITED_RESULT_CODES
 from typing import List, Type
 from .info import ProductInfo
 import logging
+from io import StringIO
+from smspdu.fields import SMSDeliver
 
 class Modem(ATModem):
 
@@ -41,15 +43,32 @@ class Modem(ATModem):
         number = response.split(',')[1].replace('"','')
         return number
 
+    def parse_message(self, index, status, alpha, length, pdu) -> SMS:
+        data = SMSDeliver.decode(StringIO(pdu.decode()))
+
+        text = str(data['user_data']['data'])
+        from_number = data['sender']['number']
+        date = data['scts']
+
+        return SMS(
+            index=index,
+            status=status,
+            alpha=alpha,
+            length=length,
+            pdu=pdu,
+            text=text,
+            from_number=from_number,
+            date=date
+        )
+    
     async def read_message(self, index: int) -> SMS:
         command = ExtendedCommand(b'AT+CMGR').write(str(index).encode())
         response = await self.send_command(command)
-        if b'+CMGR' not in bytes(responses[1]):
-            self.logger.debug(f'Message does not exist at index {index}')
-            return None
-        status, alpha, length = bytes(responses[1]).lstrip(b'+CMGR: ').split(b',')
 
-        return SMS(index, status, alpha, length, bytes(responses[2]))
+        status, alpha, length = response[0].replace(b'+CMGR: ', b'').split(b',')
+        pdu = response[1]
+
+        return self.parse_message(index, status, alpha, length, pdu)
 
     async def send_message(self, to_number: str, text: str, timeout: int = 5):
         pdus = encodeSmsSubmitPdu(to_number, text)
@@ -59,31 +78,35 @@ class Modem(ATModem):
             length = str(pdu[1]).encode()
             command = ExtendedCommand(b'AT+CMGS').write(length)
             await self.write(command)
-            await self.read(terminator=bytes(command)) # read out and discard command echo
-            await self.read(seperator=b'> ', terminator=b'') # read out and discard prompt
+            await self.read_response(terminator=bytes(command)) # read out and discard command echo
+            await self.read_response(seperator=b'> ', terminator=b'') # read out and discard prompt
             command = ExtendedCommand(pdu[0].hex().upper().encode()).execute()
             await self.write(command, terminator=chr(26).encode()) # send pdu with CTRL-Z terminator
-            responses += await self.read(timeout=timeout)
+            responses.append(await self.read_response(timeout=timeout))
         self.unlock()
         return responses
 
     async def list_messages(self, status: str = 'ALL') -> List[SMS]:
-        assert status in STATUS_MAP, \
-            KeyError(f'Invalid status {status}: {tuple(STATUS_MAP.keys())}')
+        if status not in STATUS_MAP:
+            raise ValueError(f'Invalid status {status}: {tuple(STATUS_MAP.keys())}')
         status = STATUS_MAP[status]
 
         command = ExtendedCommand(b'AT+CMGL').write(status)
-        responses = await self.send_command(command)
-        #responses = responses[1:-2]
+        response = await self.send_command(command)
+        parts = [part for part in response][:-1]
+        
+        if len(parts)%2 > 0:
+            raise ValueError(f'Expecting even number of parts in response: {response}')
 
         messages = []
-        for n in range(len(responses[1:-2])//2):
+        for n in range(len(parts)//2):
             try:
-                index, status, alpha, length = bytes(responses[1:-2][n*2]).lstrip(b'+CMGL: ').split(b',')
-                pdu = bytes(responses[1:-2][(n*2)+1])
-                messages.append(SMS(index, status, alpha, length, pdu))
+                index, status, alpha, length = message_info.replace(b'+CMGL: ', b'').split(b',')
+                pdu = parts[(n*2)+1]
+                message = self.parse_message(index, status, alpha, length, pdu)
+                messages.append(message)
             except:
-                self.logger.error([str(r) for r in responses], exc_info=True)
+                self.logger.error(f'Failed to parse message: {parts[(n*2):(n*2)+1]}', exc_info=True)
 
         return messages
 
--- async_gsm_modem/quectel_ec25/sms.py
+++ async_gsm_modem/quectel_ec25/sms.py
@@ -1,28 +1,12 @@
-from .constants import STATUS_MAP_R
-from io import StringIO
-from smspdu.fields import SMSDeliver
+from pydantic import BaseModel
+from datetime import datetime
 
-class SMS:
-
-    def __init__(self, index: bytes, status: bytes, alpha: bytes, length: bytes, pdu: bytes):
-        self.index = int(index)
-        self.status = STATUS_MAP_R[status]
-        self.alpha = alpha.decode()
-        self.length = int(length)
-        self.pdu = pdu
-
-        data = SMSDeliver.decode(StringIO(pdu.decode()))
-        self.data = data
-
-        self.text = str(data['user_data']['data'])
-        self.from_number = data['sender']['number']
-        self.date = data['scts']
-
-    def __str__(self):
-        return f'SMS(index={self.index}, ' \
-                f'status={self.status}, ' \
-                f'date={self.date.strftime("%Y-%m-%dT%H:%M:%S%Z")}, ' \
-                f'text={self.text})' \
-    
-    def to_dict(self):
-        return vars(self)
+class SMS(BaseModel):
+    index: int
+    status: str
+    alpha: str
+    length: int
+    pdu: bytes
+    text: str
+    from_number: str
+    date: datetime
