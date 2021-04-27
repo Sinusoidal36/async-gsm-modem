349b2b2c7340584fb3f2a719a3b26f853894a883
Jordan Martin <jordan.martin@callfire.com>
2021-04-27 04:53:13 +0000

    bump package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="async-gsm-modem",
-    version="0.0.1",
+    version="0.0.2",
     author="sinusoidal",
     author_email="",
     description="An async GSM modem driver library",
