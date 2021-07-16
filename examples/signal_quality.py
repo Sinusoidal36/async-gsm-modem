bba4b128bb0de4d7d24538b0965d5a1c60a7fae4
Sinusoidal <jordanma36@gmail.com>
2021-07-16 12:08:26 -0700

    implemented some more info functions

---
+++ examples/signal_quality.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+import sys
+from async_gsm_modem.quectel_ec25 import Modem
+
+async def example():
+    modem = Modem('/dev/ttyUSB2', 115200)
+    await modem.connect()
+    await modem.ping()
+    print(await modem.signal_quality())
+    await modem.close()
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(example())
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    main()
